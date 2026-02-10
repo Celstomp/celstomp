5c4c2d0d363dcab1861572a2e6607888395b9e97
hingler <jamieboy1337@gmail.com>
2026-02-09 23:34:50 -0800

    refactor celstomp app monolith into several supporting scripts this should help expedite the process of refactoring app code.

--- celstomp/js/html-loader.js
+++ celstomp/js/html-loader.js
@@ -13,7 +13,35 @@
     ];
 
     const appScripts = [
+        './js/helper-funcs.js',
+        
+        './js/time-helper.js',
+        './js/color-manager.js',
+        './js/zoom-helper.js',
+        './js/color-wheel.js',
+        './js/island-helper.js',
+        './js/layer-manager.js',
+        './js/timeline-helper.js',
+
+        
+        './js/swatch-handler.js',
+        './js/history-helper.js',
+        './js/brush-helper.js',
+        './js/canvas-helper.js',
+        './js/eraser-manager.js',
+        './js/lasso-helper.js',
+        './js/export-helper.js',
+        './js/pointer-events.js',
+
+        './js/pointer-wire.js',
+        './js/menu-wires.js',
+        './js/qol-wiring-handler.js',
+        
+        './js/dock-helper.js',
+        './js/mobile-native-zoom-guard.js',
+        './js/mount-island-dock.js',
         './js/ui-components.js',
+
         './js/omggif.js',
         './celstomp-imgseq.js',
         './celstomp-autosave.js',
